a69eec0b62bebd6282b4a2a2dda67a4129c2bf32
mudzereli <mudz@charter.net>
2025-12-22 19:32:46 -0600

    Update camera.ts

--- src/apis/camera.ts
+++ src/apis/camera.ts
@@ -6,17 +6,41 @@ import { minimap } from './minimap';
 class Camera {
   #position: Vector = new Vector(0, 0);
 
+  // 🔍 debug fields
+  #center: Vector = new Vector(0, 0);
+  #cameraPos: Vector = new Vector(0, 0);
+  #normalized: Vector = new Vector(0, 0);
+
   constructor() {
     game.on('frame_end', () => {
-      const center = Vector.add(minimap.viewportPos, Vector.unscale(2, minimap.viewportDim));
+      const center = Vector.add(
+        minimap.viewportPos,
+        Vector.unscale(2, minimap.viewportDim)
+      );
+
       const cameraPos = Vector.subtract(center, minimap.minimapPos);
       const normalized = Vector.divide(cameraPos, minimap.minimapDim);
+
+      this.#center = center;
+      this.#cameraPos = cameraPos;
+      this.#normalized = normalized;
       this.#position = arena.scale(normalized);
     });
   }
 
+  // existing
   get position(): Vector {
     return this.#position;
+  }
+
+  // 🔍 debug getters
+  get debug() {
+    return {
+      center: this.#center,
+      cameraPos: this.#cameraPos,
+      normalized: this.#normalized,
+      position: this.#position,
+    };
   }
 }
 
